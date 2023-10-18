37c18c70959666d9e2b54a015f4ab9070e25f610
jerem.re Home PC <jerem.point.re@gmail.com>
2023-10-18 14:33:19 +0400

    Thanx mates ^^
    Co-authored-by: framelelo <framelelo@users.noreply.github.com>
    
    Co-authored-by: Liebe974 <Liebe974@users.noreply.github.com>

--- viouz/omPaje.php
+++ viouz/omPaje.php
@@ -6,9 +6,6 @@ function omPaje($allStatiouz)
     $titre = "Om";
     ob_start();
 
-    ?>
-
-    <?php
     if ($izKonecTed) { ?>
         <form method="POST" action="?p=statiouz&a=kriayte">
             <textarea name="content" placeholder="Nouveau statiouz" cols="37" rows="13" required></textarea>
@@ -17,60 +14,61 @@ function omPaje($allStatiouz)
     <?php } ?>
 
     <div class="container">
-        <div class="card">
+        
             <?php foreach ($allStatiouz as $s) { ?>
-
-                    <h3>
-                        <?= "By " . guetIouzeurPseudo($s["id_users"]) ?>
-                    </h3>
-                    <p>
+                <div class="card">
+                <h3>
+                    <?= "By " . guetIouzeurPseudo($s["id_users"]) ?>
+                </h3>
+                <p>
                     <?= $s["content"] ?>
-                    </p>
+                </p>
 
-                    <?php
-                    if ($izKonecTed) { ?>
-                        <form method="POST" action="?p=kommante&a=kriayte&id_status=<?= $s["id"] ?>">
-                            <textarea name="content" placeholder="Plassoldeur du commentaire" cols="37" rows="2" required></textarea>
-                            <br><input type="submit" value="Seumite kommante">
-                        </form>
-                    <?php } ?>
-                    <?php $comments = guetOlKommantsBayiStatiouz($s["id"]);
-                    if (count($comments) <= 5) {
-                        foreach ($comments as $c) { ?>
-                            <p>
-                                <?= guetIouzeurPseudo($c["id_users"]) . " said: " ?>
-                                <?= $c["content"] ?>
-                            </p>
-                        <?php }
-                    } else {
-                        echo "<br> Too much comments, ouch!";
-                    }
-                    } ?>
+                <?php
+                if ($izKonecTed) { ?>
+                    <form method="POST" action="?p=kommante&a=kriayte&id_status=<?= $s["id"] ?>">
+                        <textarea name="content" placeholder="Plassoldeur du commentaire" cols="37" rows="2" required></textarea>
+                        <br><input type="submit" value="Seumite kommante">
+                    </form>
+                <?php } ?>
+                <?php $comments = guetOlKommantsBayiStatiouz($s["id"]);
+                if (count($comments) <= 5) {
+                    foreach ($comments as $c) { ?>
+                        <p>
+                            <?= guetIouzeurPseudo($c["id_users"]) . " said: " ?>
+                            <?= $c["content"] ?>
+                        </p>
+                    <?php }
+                } else {
+                    echo "<br> Too much comments, ouch!";
+                }
+                ?>
 
-                    <div class="layikss">
-                        <?php if ($izKonecTed) {
-                            if (izLayikedde($s["id"], $_SESSION["user"]["id_users"])) {
-                                $axssion = "diletteLayike";
-                                $value = "<ion-icon name='heart-outline' size='large'></ion-icon>";
-                            } else {
-                                $axssion = "kriayteLayike";
-                                $value = "<ion-icon name='heart-outline' size='large'></ion-icon>";
-                            }
-                            ;
-                            ?>
-                            <a href="?p=layike&a=<?= $axssion ?>&id_status=<?= $s["id"] ?> ">
-                                <?= $value ?>
-                            </a>
-                        </div>
-               <?php } ?>
-            </div>
-        </div>
+                <div class="layikss">
+                    <?php if ($izKonecTed) {
+                        
+                        if (izLayikedde($s["id"], $_SESSION["user"]["id_users"])) {
+                            $axssion = "diletteLayike";
+                            $value = "<ion-icon name='heart-outline' size='large'></ion-icon>";
+                        } else {
+                            $axssion = "kriayteLayike";
+                            $value = "<ion-icon name='heart-outline' size='large'></ion-icon>";
+                        }?>
+                        <a href="?p=layike&a=<?= $axssion ?>&id_status=<?= $s["id"] ?> ">
+                            <?= $value ?>
+                        </a>
+                        <?php } ?>
+                </div>
+               
+                </div>
+            <?php } ?>
+    
+    </div>
 
+<?php
 
-        <?php
-
-        $contenu = ob_get_clean();
-        require "yayout.php";
+    $contenu = ob_get_clean();
+    require "yayout.php";
 }
 
 ?>
